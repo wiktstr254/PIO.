69f13e705bf1d54a95c56db847fbedee280239e3
wiktstr254 <72355529+wiktstr254@users.noreply.github.com>
2022-10-20 16:59:16 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,11 +4,23 @@ import java.util.Random;
 
 public class Player {
     
-    private Random rand = new Random();
-    private String name;
+    private Random rand;
+    private String name = "Brak imienia";
+    
+    public Player() {
+        this.rand = new Random();
+    }
+    
+    public Player(String name) {
+        this.rand = new Random();
+        setName(name);
+    }
     
     public void setName(String name) {
-        this.name = name;
+        if(name != null && !name.isEmpty())
+            this.name = name;
+        else
+            System.err.println("Błędne imię");
     }
     
     public String getName() {
